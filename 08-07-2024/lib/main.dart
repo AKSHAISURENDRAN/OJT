5cdeb39785bbafdabf5bbb8195a9b40cf75905f6
AKSHAISURENDRAN <aknair1417@gmail.com>
2024-07-09 10:22:34 +0530

    flutter

---
+++ 08-07-2024/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {                 // when main function execution  occurs the app  runs !
+  runApp(
+    MaterialApp(              // Material app is a widget
+      home: Center(
+        child: Text('Hello World'),  //
+      ),
+    ),
+  ); //Material app isn a widget
+}
